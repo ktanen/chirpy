4e5b39ca03dfff4e5ff8ac9d65c3f9a435d71f81
ktanen <kevinbtanen@gmail.com>
2026-06-09 10:54:05 -0400

    Added middleware to log non-OK status codes

---
+++ src/api/middleware.ts
@@ -0,0 +1,14 @@
+import {Request, Response, NextFunction } from "express"
+
+export function middlewareLogResponses(req: Request, res: Response, next: NextFunction) {
+    res.on("finish", () => {
+        const statusCode = res.statusCode;
+
+        if (statusCode < 200 || statusCode >= 300) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${statusCode}`);
+        }        
+    });
+
+    next();
+
+}
