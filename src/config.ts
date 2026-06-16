20dfa18ae293bc5d0d1d5d0c65f7badc9d744891
ktanen <kevinbtanen@gmail.com>
2026-06-16 14:51:48 -0400

    Added 'Create User' query

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,24 @@
+
+process.loadEnvFile();
+
 type APIConfig = {
   fileserverHits: number;
+  dbURL: string;
 };
+
+function envOrThrow(key: string): string {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`The ${key} environment variable is missing`);
+  }
+  return value;
+}
+  
+const dbURL = envOrThrow("DB_URL");
 
 const config: APIConfig = {
     fileserverHits: 0,
+    dbURL: dbURL
 };
 
 export {config};
