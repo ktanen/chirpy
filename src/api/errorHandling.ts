a7e7d253c775b1c150e8a120fa8d7db4d67ead4c
ktanen <kevinbtanen@gmail.com>
2026-06-16 09:08:46 -0400

    Changed error handling file to match solution file's cleaner code

--- src/api/errorHandling.ts
+++ src/api/errorHandling.ts
@@ -1,13 +1,14 @@
 import {Request, Response, NextFunction } from "express"
-
+import { respondWithError } from "./json.js";
 export function errorHandler(
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction,
 ) {
-    console.log(err);
-    res.status(500).json({
-    "error": "Something went wrong on our end"
-    })
+    let statusCode = 500;
+    let message = "Something went wrong on our end";
+    console.log(err.message);
+
+    respondWithError(res, statusCode, message);
 }
